7315692e6f57093c9bb95597ce356fe54364a04c
George Abboud <gtabboud@gmail.com>
2020-05-29 14:46:37 -0400

    updates to menusmanager

--- ccnavmenus/force-app/main/default/lwc/menusManager/menusManager.js
+++ ccnavmenus/force-app/main/default/lwc/menusManager/menusManager.js
@@ -2,14 +2,17 @@ import { LightningElement, track, wire } from 'lwc';
 import fetchMenus from '@salesforce/apex/menusManagerController.getMenus';
 import deleteMenu from '@salesforce/apex/menusManagerController.deleteMenu';
 import fetchMenu from '@salesforce/apex/menusManagerController.getMenu';
+import deleteMenuItem from '@salesforce/apex/menusManagerController.deleteMenuItem';
 import { refreshApex } from '@salesforce/apex';
 import {loadStyle} from 'lightning/platformResourceLoader';
 import menusManagerCSS from '@salesforce/resourceUrl/menusManager';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 
 const MENU_ITEM_ACTIONS = [
     { label: 'Create Child Menu Item', name: 'createMenuItem' },
-    { label: 'Edit', name: 'edit' },
-    { label: 'Delete', name: 'delete' }
+    { label: 'Edit Menu Item', name: 'editMenuItem' },
+    { label: 'Delete Menu Item', name: 'deleteMenuItem' }
 ];
 
 
@@ -26,6 +29,24 @@ const MENU_ITEM_COLUMNS_DEFINITION = [
         label: 'URL',
         initialWidth: 300,
     },
+    {
+        type: 'text',
+        fieldName: 'position',
+        label: 'Position',
+        initialWidth: 100,
+    },
+    {
+        type: 'text',
+        fieldName: 'openInNewWindow',
+        label: 'Opens in New Window',
+        initialWidth: 100,
+    },
+    {
+        type: 'text',
+        fieldName: 'isPublic',
+        label: 'Public',
+        initialWidth: 100,
+    },
     { 
         type: 'action', 
         typeAttributes: 
@@ -41,21 +62,47 @@ export default class MenusManager extends LightningElement {
     @track createModalOpen = false;
     @track editModalOpen = false;
     @track deleteModalOpen = false;
+    @track deleteMIModalOpen = false;
+    @track createEditModalOpen = false;
     @track menuList;
     @track menuListResult;
     @track menuOptions;
     @track menuItemList;
-    @track menuId;
+    @track menuItemListResult;
+    @track menuId = null;
     @track menuName;
+    @track selectedMenuItemIdForCreate;
+    @track selectedMenuItemIdForEdit;
+    @track selectedMenuItemLabelForEdit;
+    @track selectedMenuItemIdForDelete;
+    @track selectedMenuItemLabelForDelete;
     @track error;
 
     MENU_ITEM_COLUMNS_DEFINITION = MENU_ITEM_COLUMNS_DEFINITION;
 
 
     //wire functions
+    wireFetchMenu;
+    @wire(fetchMenu,{menuId: '$menuId'})
+    fetchMenuImperativeWiring(result) 
+    {
+        if (result.data) {
+            try {
+                this.menuItemListResult = result;
+                this.menuItemList = JSON.parse(result.data);
+                this.error = undefined;
+            }catch(e){}
+        } else if (result.error) {
+            this.menuItemListResult = result;
+            this.error = result.error;
+            this.menuItemList = undefined;
+        }
+
+    }
+
     wireFetchMenus;
     @wire(fetchMenus)
-    imperativeWiring(result) 
+    fetchMenusImperativeWiring(result) 
     {
         if (result.data) {
             this.menuListResult = result;
@@ -70,24 +117,36 @@ export default class MenusManager extends LightningElement {
             }
             this.error = undefined;
         } else if (result.error) {
+            this.menuListResult = result;
             this.error = result.error;
             this.menuList = undefined;
         }
     }
+
+    
+    
 
     connectedCallback() 
     {
         loadStyle(this, menusManagerCSS);
     }
 
+    renderedCallback()
+    {
+        try{
+            let grid =  this.template.querySelector( ".menuItemstreegrid" );
+            grid.expandAll();
+        }catch(e){}
+    }
+
     openCreateModal() 
     {
-        this.createModalOpen = true
+        this.createModalOpen = true;
     }
     
     closeCreateModal() 
     {
-        this.createModalOpen = false
+        this.createModalOpen = false;
     } 
     
     createMenuSuccess() 
@@ -98,12 +157,12 @@ export default class MenusManager extends LightningElement {
 
     openEditModal() 
     {
-        this.editModalOpen = true
+        this.editModalOpen = true;
     }
     
     closeEditModal() 
     {
-        this.editModalOpen = false
+        this.editModalOpen = false;
     } 
     
     editMenuSuccess() 
@@ -114,12 +173,12 @@ export default class MenusManager extends LightningElement {
 
     openDeleteModal() 
     {
-        this.deleteModalOpen = true
+        this.deleteModalOpen = true;
     }
     
     closeDeleteModal() 
     {
-        this.deleteModalOpen = false
+        this.deleteModalOpen = false;
     } 
     
     handleDeleteMenu() 
@@ -128,10 +187,74 @@ export default class MenusManager extends LightningElement {
         deleteMenu({
             menuId: this.menuId
         })
+        .then((result) => {
+            if(result === 'success')
+            {
+                this.closeDeleteModal();
+                this.menuItemList = undefined;
+                this.menuId = null;
+                return refreshApex(this.menuListResult);
+            }
+            else 
+            {
+                this.showNotification('Error Deleting Menu!', result+'','error');
+            }
+        })
+        .catch((error) => {
+            this.message = 'Error received: code' + error.errorCode + ', ' +
+                'message ' + error.body.message;
+        });
+
+        
+    }
+
+    openCreateEditModal() 
+    {
+        this.createEditModalOpen = true;
+    }
+    
+    closeCreateEditModal() 
+    {
+        this.createEditModalOpen = false;
+        this.selectedMenuItemIdForCreate = undefined;
+        this.selectedMenuItemIdForEdit = undefined;
+        this.selectedMenuItemLabelForEdit = undefined;
+    } 
+
+    handleCreateEditSuccess(e)
+    {
+
+        refreshApex(this.menuItemListResult);
+
+        this.selectedMenuItemIdForCreate = undefined;
+        this.selectedMenuItemIdForEdit = undefined;
+        this.selectedMenuItemLabelForEdit = undefined;
+        this.closeCreateEditModal();
+
+    }
+
+    openDeleteMIModal() 
+    {
+        this.deleteMIModalOpen = true;
+    }
+    
+    closeDeleteMIModal() 
+    {
+        this.selectedMenuItemIdForDelete = undefined;
+        this.selectedMenuItemLabelForDelete = undefined;
+        this.deleteMIModalOpen = false;
+    } 
+    
+    handleDeleteMIMenu() 
+    {
+
+        deleteMenuItem({
+            menuItemId: this.selectedMenuItemIdForDelete
+        })
         .then(() => {
             
-            this.closeDeleteModal();
-            return refreshApex(this.menuListResult);
+            this.closeDeleteMIModal();
+            return refreshApex(this.menuItemListResult);
         })
         .catch((error) => {
             this.message = 'Error received: code' + error.errorCode + ', ' +
@@ -152,23 +275,7 @@ export default class MenusManager extends LightningElement {
             }
         }
         
-        fetchMenu({
-            menuId: this.menuId
-        })
-        .then((result) => {
-            
-            this.menuItemList = JSON.parse(JSON.stringify(JSON.parse(result)).split('_children').join('_children'));
-            
-            
-        })
-        .catch((error) => {
-            this.message = 'Error received: code' + error.errorCode + ', ' +
-                'message ' + error.body.message;
-        })
-        .finally(() => {
-            const grid =  this.template.querySelector( ".menuItemstreegrid" );
-            grid.expandAll();
-        });
+        refreshApex(this.menuItemListResult);        
 
     }
 
@@ -177,15 +284,32 @@ export default class MenusManager extends LightningElement {
         const row = event.detail.row;
         switch (action.name) {
             case 'createMenuItem':
-                alert('Showing Details: ' + JSON.stringify(row));
+                this.selectedMenuItemIdForCreate = row.id;
+                this.selectedMenuItemIdForEdit = undefined;
+                this.selectedMenuItemLabelForEdit = undefined;
+                this.openCreateEditModal();
                 break;
-            case 'delete':
-                const rows = this.data;
-                const rowIndex = rows.indexOf(row);
-                rows.splice(rowIndex, 1);
-                this.data = rows;
+            case 'editMenuItem':
+                this.selectedMenuItemIdForCreate = undefined;
+                this.selectedMenuItemIdForEdit = row.id;
+                this.selectedMenuItemLabelForEdit = row.label;
+                this.openCreateEditModal();
+                break;
+            case 'deleteMenuItem':
+                this.selectedMenuItemIdForDelete = row.id;
+                this.selectedMenuItemLabelForDelete = row.label;
+                this.openDeleteMIModal();
                 break;
         }
+    }
+
+    showNotification(title, message, variant) {
+        const evt = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+        this.dispatchEvent(evt);
     }
 
 }
